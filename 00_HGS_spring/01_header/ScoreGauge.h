af5e447c7a705388615855e51a16849288185058
坂本翔唯 <221093@st.yoshida-g.ac.jp>
2023-03-23 13:00:26 +0900

    スコアゲージの追加

---
+++ 00_HGS_spring/01_header/ScoreGauge.h
@@ -0,0 +1,12 @@
+#ifndef _SCOREGAUGE_H_
+#define _SCOREGAUGE_H_
+
+#include "main.h"
+
+//プロトタイプ宣言
+void InitScoreGauge(void);
+void UninitScoreGauge(void);
+void UpdateScoreGauge(void);
+void DrawScoreGauge(void);
+void SetScoreGauge(void);
+#endif
